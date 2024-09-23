f474a70cb388a31a2aa3694c1b32de43943ccee1
이연승 <leeys951006@gmail.com>
2024-09-23 18:01:42 +0900

    햄버거 메뉴 오류 수정

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -63,7 +63,7 @@ export default function Header() {
             </svg>
           ) : (
             <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           )}
         </button>
