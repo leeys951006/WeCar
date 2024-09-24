a46710b40ffd4d212bec5ff7e87f3e1a384e6b65
이연승 <leeys951006@gmail.com>
2024-09-24 17:32:16 +0900

    위카 모빌리티의 핵심 서비스 부분 작업

---
+++ my-app/app/components/HomeContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeContent: React.FC = () => {
+  return (
+    <div className="bg-white py-4 text-left px-8 mt-20 ml-40">
+      <h1 className="text-5xl font-bold mb-6 text-black">위카 모빌리티의 핵심 서비스</h1>
+      <p className="text-2xl text-black">위카만의 전문화된 자동차 서비스를 통해 편리한 자동차 생활을 누리세요.</p>
+    </div>
+  );
+};
+
+export default HomeContent;
