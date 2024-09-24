e09c9b8658dc3de06cef18f791376131511ecaf5
이연승 <leeys951006@gmail.com>
2024-09-24 12:16:44 +0900

    vercel.svg 추가

--- my-app/app/components/Footer.tsx
+++ my-app/app/components/Footer.tsx
@@ -1,4 +1,4 @@
 // components/Footer.tsx
 export default function Footer() {
-  return <footer className="bg-gray-100 p-5 text-center">{/* Footer content will go here */}</footer>;
+  return <footer className="bg-[#282828] p-5 text-center">{/* Footer content will go here */}</footer>;
 }
