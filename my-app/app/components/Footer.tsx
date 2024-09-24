41e2e8114a62c1cf19eac8319c6d6cf7e2473642
이연승 <leeys951006@gmail.com>
2024-09-24 13:10:34 +0900

    Footer 작업 완료

--- my-app/app/components/Footer.tsx
+++ my-app/app/components/Footer.tsx
@@ -1,4 +1,60 @@
 // components/Footer.tsx
 export default function Footer() {
-  return <footer className="bg-[#282828] p-5 text-center">{/* Footer content will go here */}</footer>;
+  return (
+    <footer className="bg-[#282828] p-8 text-left text-white flex flex-col md:flex-row">
+      {/* First Section */}
+      <div className="flex-1 flex flex-col ml-0 md:ml-20 mt-8 md:mb-5">
+        <p className="mb-1 text-gray-400 text-xs md:text-sm" style={{ color: '#686868' }}>
+          Copyright © 위카모빌리티 All Rights Reserved.
+        </p>
+        <div className="mb-8">
+          <a href="/terms-of-use" className="text-[#686868] hover:text-white hover:underline mr-3 text-xs md:text-sm">
+            Terms of Use
+          </a>
+          <span className="text-[#686868] mr-3 text-xs md:text-sm">|</span>
+          <a href="/privacy-policy" className="text-[#686868] hover:text-white hover:underline text-xs md:text-sm">
+            Privacy Policy
+          </a>
+        </div>
+        <div className="flex space-x-4 ml-0 md:ml-2 mb-5">
+          <div className="relative group">
+            <a href="/google">
+              <img src="/google.png" alt="Google" className="w-full max-w-xs md:max-w-[200px] h-auto" />
+            </a>
+            <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+          </div>
+          <div className="relative group">
+            <a href="/apple">
+              <img src="/apple.png" alt="Apple" className="w-full max-w-xs md:max-w-[200px] h-auto" />
+            </a>
+            <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+          </div>
+        </div>
+      </div>
+
+      {/* Second Section */}
+      <div className="flex-1 flex flex-col items-start md:items-end mr-0 md:mr-20 mt-8 md:mt-8">
+        <div className="flex space-x-4 mb-4">
+          <a href="/blog">
+            <img src="/blog_icon.png" alt="Blog" className="w-8 h-8" />
+          </a>
+          <a href="/facebook">
+            <img src="/facebook_icon.png" alt="Facebook" className="w-8 h-8" />
+          </a>
+          <a href="/instagram">
+            <img src="/insta_icon.png" alt="Instagram" className="w-8 h-8" />
+          </a>
+          <a href="/education">
+            <img src="/edu_icon1.png" alt="Education" className="w-8 h-8" />
+          </a>
+        </div>
+        <div className="text-sm text-left md:text-right pt-0.5" style={{ color: '#686868' }}>
+          <p className="text-xs md:text-sm">자동차진단＆탁송서비스판매 ㅣ 위카모빌리티(주) ㅣ 대표자 정태영 이승범</p>
+          <p className="text-xs md:text-sm">사업자등록번호 : 755-81-02354 ㅣ 통신판매업신고 제2023-용인기흥-6932호</p>
+          <p className="text-xs md:text-sm">본점 : 경기도 용인시 기흥구 중부대로 242 A동 W116호~117호 / 안성 : 경기도 안성시 공도읍 문터길 60</p>
+          <p className="text-xs md:text-sm">대표번호 진단 1544-2165 탁송 1544-5286 ㅣ 이메일 WECAR@WECAR-M.CO.KR</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
