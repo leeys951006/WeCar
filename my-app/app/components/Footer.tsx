9a9acce0316f4bbf67af0ac5f4d863011d6f4761
이연승 <leeys951006@gmail.com>
2024-09-26 02:19:07 +0900

    footer 버튼 수정

--- my-app/app/components/Footer.tsx
+++ my-app/app/components/Footer.tsx
@@ -18,13 +18,13 @@ export default function Footer() {
         </div>
         <div className="flex space-x-4 ml-0 md:ml-2 mb-5">
           <div className="relative group">
-            <a href="/google">
+            <a href="https://play.google.com/store/apps/details?id=co.kr.wecarmobility.app&pli=1">
               <img src="/google.png" alt="Google" className="w-full max-w-xs md:max-w-[200px] h-auto" />
             </a>
             <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
           </div>
           <div className="relative group">
-            <a href="/apple">
+            <a href="https://apps.apple.com/kr/app/%EC%9C%84%EC%B9%B4%EB%AA%A8%EB%B9%8C%EB%A6%AC%ED%8B%B0-%EC%98%88%EC%95%BD%EC%8B%9C%EC%8A%A4%ED%85%9C/id6451229725">
               <img src="/apple.png" alt="Apple" className="w-full max-w-xs md:max-w-[200px] h-auto" />
             </a>
             <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
@@ -35,18 +35,26 @@ export default function Footer() {
       {/* Second Section */}
       <div className="flex-1 flex flex-col items-start md:items-end mr-0 md:mr-20 mt-8 md:mt-8">
         <div className="flex space-x-4 mb-4">
-          <a href="/blog">
-            <img src="/blog_icon.png" alt="Blog" className="w-8 h-8" />
-          </a>
-          <a href="/facebook">
-            <img src="/facebook_icon.png" alt="Facebook" className="w-8 h-8" />
-          </a>
-          <a href="/instagram">
-            <img src="/insta_icon.png" alt="Instagram" className="w-8 h-8" />
-          </a>
-          <a href="/education">
-            <img src="/edu_icon1.png" alt="Education" className="w-8 h-8" />
-          </a>
+          <div className="relative group">
+            <a href="https://blog.naver.com/wecarmobility">
+              <img src="/blog_icon.png" alt="Blog" className="w-8 h-8 transition-opacity duration-300 hover:opacity-60" />
+            </a>
+          </div>
+          <div className="relative group">
+            <a href="https://www.facebook.com/wecarm">
+              <img src="/facebook_icon.png" alt="Facebook" className="w-8 h-8 transition-opacity duration-300 hover:opacity-60" />
+            </a>
+          </div>
+          <div className="relative group">
+            <a href="https://www.instagram.com/wecarmobility/">
+              <img src="/insta_icon.png" alt="Instagram" className="w-8 h-8 transition-opacity duration-300 hover:opacity-60" />
+            </a>
+          </div>
+          <div className="relative group">
+            <a href="https://wecarm.liveklass.com/">
+              <img src="/edu_icon1.png" alt="Education" className="w-8 h-8 transition-opacity duration-300 hover:opacity-60" />
+            </a>
+          </div>
         </div>
         <div className="text-sm text-left md:text-right pt-0.5" style={{ color: '#686868' }}>
           <p className="text-xs md:text-sm">자동차진단＆탁송서비스판매 ㅣ 위카모빌리티(주) ㅣ 대표자 정태영 이승범</p>
