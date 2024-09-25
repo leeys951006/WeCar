be422ff948839c1a621c1cf7b197012d9a58a882
이연승 <leeys951006@gmail.com>
2024-09-26 01:22:28 +0900

    HomeContent2.tsx 생성

---
+++ my-app/app/components/HomeContent2.tsx
@@ -0,0 +1,16 @@
+// components/HomeContent2.tsx
+
+import React from 'react';
+
+const HomeContent2: React.FC = () => {
+  return (
+    <div className="p-8 bg-gray-100">
+      <h2 className="text-2xl font-bold mb-4">Home Content 2</h2>
+      <p>
+        이곳은 HomeContent2 컴포넌트입니다. 이곳에 원하는 내용을 추가하세요.
+      </p>
+    </div>
+  );
+};
+
+export default HomeContent2;
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 import Slider from './components/Slider'; // 슬라이드 컴포넌트 불러오기
-import HomeContent from './components/HomeContent1'; // 새로 생성한 컴포넌트 불러오기
-
+import HomeContent1 from './components/HomeContent1'; // 기존 컴포넌트 불러오기
+import HomeContent2 from './components/HomeContent2'; // 새로 생성한 컴포넌트 불러오기
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +9,8 @@ const Home: NextPage = () => {
       <div className="h-80 md:h-[80vh] md:flex md:items-center md:justify-center bg-white">
         <Slider /> {/* 슬라이드 컴포넌트 사용 */}
       </div>
-      <HomeContent /> {/* 새로 생성한 컴포넌트 사용 */}
+      <HomeContent1 /> {/* 기존 컴포넌트 사용 */}
+      <HomeContent2 /> {/* 새로 생성한 컴포넌트 사용 */}
     </div>
   );
 };
