1a7d2c252cac392b9fbb3fd68488bdf3fd64c666
이연승 <leeys951006@gmail.com>
2024-09-25 22:37:57 +0900

    HomeContent2.tsx 생성

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,16 +1,16 @@
 import { NextPage } from 'next';
 import Slider from './components/Slider'; // 슬라이드 컴포넌트 불러오기
 import HomeContent from './components/HomeContent1'; // 새로 생성한 컴포넌트 불러오기
+import HomeContent2 from './components/HomeContent2'; // 새로운 컴포넌트 불러오기
 
 const Home: NextPage = () => {
   return (
     <div className="bg-white">
-      {' '}
-      {/* 부모 요소의 배경색을 흰색으로 설정 */}
       <div className="h-80 md:h-[80vh] md:flex md:items-center md:justify-center bg-white">
         <Slider /> {/* 슬라이드 컴포넌트 사용 */}
       </div>
       <HomeContent /> {/* 새로 생성한 컴포넌트 사용 */}
+      <HomeContent2 /> {/* 새로운 컴포넌트 사용 */}
     </div>
   );
 };
